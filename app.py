136fd9a3423c723401f598763d2cebc6c8d07326
Priyanka Soni <Priyankasoni2016@gmail.com>
2024-04-29 21:48:39 +0530

    app.py modified

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ from torch.nn import sequential, Linear, SELU, Dropout, LogSigmoid
 from PIL import Image
 from torchvision.transforms import Compose, ToTensor, Resize
 from torchvision.models import resnet50
+import os
+UPLOAD_FOLDER = os.path.join('static', 'photos')
+app = flask.Flask(__name__, template_folder = 'templates')
